be9e5d03641d1b4815805c0eb6855e60089146c1
Victor J Bozzelle <victorjbozzelle@gmail.com>
2019-04-04 20:58:00 -0500

    Completed

---
+++ CarDealership/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
+
+namespace CarDealership.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+         =>  View(VehicleRepository.GetAll());
+    }
+}
---
+++ CarDealership/Models/VehicleRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealership.Models
+{
+    public class VehicleRepository
+    {
+        public static IEnumerable<Vehicle> GetAll()
+        {
+            var initialItems = new[]
+            {
+            new Vehicle { Make = "Nissan", Model = "Skyline R34", Year = 1999, Mileage = 155750 },
+            new Vehicle { Make = "Chevrolet", Model = "El Camino", Year = 1981 },
+            new Vehicle { Make = "Hyundai", Model = "Sonata 2.0t Limited", Year = 2013, Mileage = 210000 },
+            new Vehicle { Make = "Mitsubishi", Model = "Eclipse GST", Year = 1995 }
+            };
+            return initialItems;
+        }
+    }
+}
